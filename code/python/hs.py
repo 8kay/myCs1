12363ae33183faf89498a1c0c263c39b910563c8
Kate Doughty <ksd4ww@virginia.edu>
2015-11-08 23:55:47 -0500

    ok

--- code/python/hs.py
+++ code/python/hs.py
@@ -1,14 +1,11 @@
-procedure hs
-if (n = 1)
-    print
-    print none
-
-else while (n != 1)
+def hs (n):
+    l = 1
+    while (n != 1):
+        print (n)
+        if (n % 2 == 0):
+            n = n // 2
+        else:
+            n = 3*n + 1
+        l = l + 1
     print (n)
-    if (n % 2 = 0)
-        n = n//2
-    else
-        n = 3 * n + 1
-
-        
-    
+    print (l)
